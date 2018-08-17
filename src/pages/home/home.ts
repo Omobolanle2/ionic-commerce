a492f72f7b161be1674a8600ca5d654283370ea6
Omobolanle2 <omobolanlearogundade@gmail.com>
2018-08-17 10:33:29 +0100

    menu added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as woo from 'woocommerce-api';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  wooCommerce: any;
   constructor(public navCtrl: NavController) {
-
+    this.wooCommerce = woo({
+      url: 'https://busolami.000webhostapp.com/',
+      consumerKey: 'ck_e3a0788716d789e411213d9f0d9e0085e3279413',
+      consumerSecret: 'cs_0a7880c5de8f6a800591c7c6eead4ff8df1092dc  ',
+      wpAPI: true,
+      version: 'wc/v1'
+    });
+    
+    
+    this.wooCommerce.getAsync('products')
+    .then((data)=>{
+      console.log(data)
+    }), 
+    (err)=>{
+      console.log(err);
+    }
+      ;
+  
   }
+
+
 
 }
